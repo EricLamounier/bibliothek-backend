f1bbda1ce3f351b07a1bd6cf9c4345f1aad90dba
Eric Lamounier <68248491+EricLamounier@users.noreply.github.com>
2025-11-18 15:54:50 -0300

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,6 @@
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
 import { Client, types } from "pg";
-import cron from 'node-cron';
 
 // int2 (smallint)
 types.setTypeParser(21, (val) => parseInt(val, 10));
@@ -21,17 +20,6 @@ async function connect() {
     console.error("Erro ao conectar:", err);
   }
 }
-
-// Executa todo dia à meia-noite
-cron.schedule("0 0 * * *", async () => {
-  const now = new Date(Date.now()).toISOString();
-  try {
-    await pool.query("DELETE FROM REFRESH_TOKENS WHERE expira_em <= $1", [now]);
-    console.log(`[CRON] Hashes expirados removidos às ${now}`);
-  } catch (err) {
-    console.error("[CRON] Erro ao remover hashes expirados:", err);
-  }
-});
 
 connect();
 
